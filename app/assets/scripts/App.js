190034a9a529c4c32ce4643841d325fac01fe653
Edwin Chen <edwinchen85@gmail.com>
2017-08-26 16:04:44 +0800

    Add babel loaders and use ES6 flavor syntax in scripts

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,13 @@
-var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "green");
 jane.greet();
